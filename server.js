223ee7d3c1329ec088a4d7c7d084e619333c255d
Dharmesh <raikwardharmesh67@gmail.com>
2025-03-31 18:43:10 +0530

    changes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Our common Server is running!");
 });
+app.get("/Can", (req, res) => {
+  res.send("Our common Server is Can Change!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
